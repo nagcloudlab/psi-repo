ed97827be08df91c69ce182ca578471bd5a8871f
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-09-07 12:06:42 +0530

    fix

--- todos-service/src/main/java/com/example/TodosServiceApplication.java
+++ todos-service/src/main/java/com/example/TodosServiceApplication.java
@@ -10,6 +10,12 @@ public class TodosServiceApplication {
 
 	@Value("${password}")
 	private String password;
+
+	public void doSomething() {
+		String s=null;
+		String ss=s.toUpperCase();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TodosServiceApplication.class, args);
 	}
