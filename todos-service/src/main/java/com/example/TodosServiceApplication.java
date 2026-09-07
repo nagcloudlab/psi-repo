30a4b60ce0416fa23055464fbb25ea7e4f7f83da
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-09-07 11:53:16 +0530

    removed foo feature

--- todos-service/src/main/java/com/example/TodosServiceApplication.java
+++ todos-service/src/main/java/com/example/TodosServiceApplication.java
@@ -4,24 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-class Foo{
-	public Foo() {
-	}
-	public void doSomething() {
-	}
-}
 
 @SpringBootApplication
 public class TodosServiceApplication {
 
 	@Value("${password}")
 	private String password;
-
-	public void doFoo(){
-		Foo foo = null;
-		foo.doSomething();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodosServiceApplication.class, args);
 	}
