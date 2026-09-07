3277652b903e1c540ddbb627d9ca27061e833c78
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-09-07 10:27:19 +0530

    with gitleaks stage

--- todos-service/src/main/java/com/example/TodosServiceApplication.java
+++ todos-service/src/main/java/com/example/TodosServiceApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodosServiceApplication {
 
+	private static final String APPLICATION_NAME = "Todos Service";
+	private String password;;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TodosServiceApplication.class, args);
 	}
